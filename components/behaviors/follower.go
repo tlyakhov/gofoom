27b8c807b9d5e604c2e390b7e1da8e5bd2dc903e
Tim Lyakhovetskiy <tlyakhov@gmail.com>
2024-08-27 22:36:25 -0700

    Create path following behavior. Fix really bad detaching bugs in ECS. Add some more comments.

---
+++ components/behaviors/follower.go
@@ -0,0 +1,87 @@
+// Copyright (c) Tim Lyakhovetskiy
+// SPDX-License-Identifier: MPL-2.0
+
+package behaviors
+
+import (
+	"strconv"
+	"tlyakhov/gofoom/ecs"
+)
+
+type Follower struct {
+	ecs.Attached `editable:"^"`
+	Path         ecs.Entity            `editable:"Path" edit_type:"Path"`
+	NoZ          bool                  `editable:"2D only"`
+	Lifetime     ecs.AnimationLifetime `editable:"Lifetime"`
+	Speed        float64               `editable:"Speed"`
+
+	// Internal state
+	Index int
+}
+
+var FollowerComponentIndex int
+
+func init() {
+	FollowerComponentIndex = ecs.RegisterComponent(&ecs.Column[Follower, *Follower]{Getter: GetFollower})
+}
+
+func GetFollower(db *ecs.ECS, e ecs.Entity) *Follower {
+	if asserted, ok := db.Component(e, FollowerComponentIndex).(*Follower); ok {
+		return asserted
+	}
+	return nil
+}
+
+func (f *Follower) String() string {
+	return "Follower"
+}
+
+func (f *Follower) Construct(data map[string]any) {
+	f.Attached.Construct(data)
+	f.NoZ = false
+	f.Index = 0
+	f.Speed = 10
+	f.Lifetime = ecs.AnimationLifetimeLoop
+
+	if data == nil {
+		return
+	}
+
+	if v, ok := data["Path"]; ok {
+		f.Path, _ = ecs.ParseEntity(v.(string))
+	}
+	if v, ok := data["Index"]; ok {
+		if i, err := strconv.Atoi(v.(string)); err == nil {
+			f.Index = int(i)
+		}
+	}
+	if v, ok := data["NoZ"]; ok {
+		f.NoZ = v.(bool)
+	}
+	if v, ok := data["Lifetime"]; ok {
+		if l, err := ecs.AnimationLifetimeString(v.(string)); err == nil {
+			f.Lifetime = l
+		}
+	}
+	if v, ok := data["Speed"]; ok {
+		f.Speed = v.(float64)
+	}
+}
+
+func (f *Follower) Serialize() map[string]any {
+	result := f.Attached.Serialize()
+
+	result["Path"] = f.Path.String()
+	result["Speed"] = f.Speed
+
+	if f.Index != 0 {
+		result["Index"] = strconv.Itoa(f.Index)
+	}
+	if f.NoZ {
+		result["NoZ"] = f.NoZ
+	}
+	if f.Lifetime != ecs.AnimationLifetimeLoop {
+		result["Lifetime"] = f.Lifetime
+	}
+	return result
+}
