2da65d244089453bd35698e46e51832d6cf13fc1
Tim Lyakhovetskiy <tlyakhov@gmail.com>
2025-01-01 22:25:03 -0800

    Refactor new slope stuff to use proper spherical coordinate naming [Editor] Fix deadlock when auto-portalling

--- editor/actions/move_surface.go
+++ editor/actions/move_surface.go
@@ -4,7 +4,6 @@
 package actions
 
 import (
-	"math"
 	"tlyakhov/gofoom/concepts"
 	"tlyakhov/gofoom/ecs"
 	"tlyakhov/gofoom/editor/state"
@@ -14,8 +13,8 @@ type SurfaceMode int
 
 const (
 	SurfaceModeLevel SurfaceMode = iota
-	SurfaceModeXYAngle
-	SurfaceModeSlopeAngle
+	SurfaceModePhi
+	SurfaceModeTheta
 )
 
 type MoveSurface struct {
@@ -25,28 +24,6 @@ type MoveSurface struct {
 	Mode     SurfaceMode
 	Floor    bool
 	Delta    float64
-}
-
-// Adapted from https://stackoverflow.com/questions/9038392/how-to-round-a-2d-vector-to-nearest-15-degrees?rq=4
-func snapIncXY(v *concepts.Vector3, deg float64, inc float64) {
-	l2d := math.Sqrt(v[0]*v[0] + v[1]*v[1])
-	snapAngle := deg * concepts.Deg2rad
-	angle := math.Atan2(v[1], v[0])
-	newAngle := (math.Round(angle/snapAngle) + inc) * snapAngle
-	v[0] = math.Cos(newAngle) * l2d
-	v[1] = math.Sin(newAngle) * l2d
-}
-
-func snapIncSlope(v *concepts.Vector3, deg float64, inc float64) {
-	l2d := math.Sqrt(v[0]*v[0] + v[1]*v[1])
-	l3d := v.Length()
-	snapAngle := deg * concepts.Deg2rad
-	angle := math.Atan2(v[2], l2d)
-	newAngle := (math.Round(angle/snapAngle) + inc) * snapAngle
-	xyAngle := math.Atan2(v[1], v[0])
-	v[0] = math.Cos(xyAngle) * math.Cos(newAngle) * l3d
-	v[1] = math.Sin(xyAngle) * math.Cos(newAngle) * l3d
-	v[2] = math.Sin(newAngle) * l3d
 }
 
 func (a *MoveSurface) Activate() {
@@ -67,7 +44,7 @@ func (a *MoveSurface) Undo() {
 		}
 
 		switch a.Mode {
-		case SurfaceModeXYAngle, SurfaceModeSlopeAngle:
+		case SurfaceModePhi, SurfaceModeTheta:
 			plane.Normal = a.Original[i].(concepts.Vector3)
 		default:
 			plane.Z.Spawn = a.Original[i].(float64)
@@ -93,13 +70,13 @@ func (a *MoveSurface) Redo() {
 		}
 
 		switch a.Mode {
-		case SurfaceModeXYAngle:
+		case SurfaceModePhi:
 			a.Original = append(a.Original, plane.Normal)
-			snapIncXY(&plane.Normal, 5, a.Delta)
+			plane.Normal.SphericalQuantIncPhi(5*concepts.Deg2rad, a.Delta)
 			plane.Normal.NormSelf()
-		case SurfaceModeSlopeAngle:
+		case SurfaceModeTheta:
 			a.Original = append(a.Original, plane.Normal)
-			snapIncSlope(&plane.Normal, 5, a.Delta)
+			plane.Normal.SphericalQuantIncTheta(5*concepts.Deg2rad, a.Delta)
 			plane.Normal.NormSelf()
 		default:
 			a.Original = append(a.Original, plane.Z.Spawn)
